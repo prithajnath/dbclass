dba0595268c83249f3d0cc6ddaf7749c153447b6
Prithaj Nath <prithajnath@gmail.com>
2025-10-07 14:49:00 -0400

    fix deps for vercel deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
-}
+  title: "dbclass",
+  description: "SQL interfaces for HW questions",
+};
 
-const geistSans = GeistSans.variable
-const geistMono = GeistMono.variable
+const geistSans = GeistSans.variable;
+const geistMono = GeistMono.variable;
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={`${geistSans} ${geistMono} antialiased dark`}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
